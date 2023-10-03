46ee6b5439bda467e875c4931cfeeb22253e5d6e
cmbayne10 <cmbayne10@gmail.com>
2023-10-03 17:10:23 -0400

    yo peeps

--- my-script.sh
+++ my-script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo Hello World, how ya'll doin,
 if you are reading this than you are probably 
-about to get hacked (kidding)
+about to get hacked (kidding). any way I hope 
+you have a great day and don't get hacked 
+unless its me.
