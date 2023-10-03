5cbe76d4b58f3bb2d50cbb951e4c385a724153ca
cmbayne10 <cmbayne10@gmail.com>
2023-10-03 17:27:27 -0400

    hi

--- my-script.sh
+++ my-script.sh
@@ -3,4 +3,4 @@ echo Hello World, how ya'll doin,
 if you are reading this than you are probably 
 about to get hacked (kidding). any way I hope 
 you have a great day and don't get hacked 
-unless its me.
+unless its me. hi there 
